c16bd9e5b96d69e428d1b83dfeefb45f1062e888
vanelozano7312 <vane.lozano7312@gmail.com>
2022-09-08 23:39:03 -0500

    Functional shift encrypt

--- cryptosystem/views.py
+++ cryptosystem/views.py
@@ -8,19 +8,29 @@ from main import *
 def cryptosystem_view(request, name=None):
     cryptosystem_obj = None
     view = "cryptosystem.html"
-    
+    context = {}
     if name is not None:
         cryptosystem_obj = Cryptosystem.objects.get(name=name)
+        context['name']= cryptosystem_obj.name
+        context['description']= cryptosystem_obj.description
+        encrypted = False
+        
         if name == "Shift":
             view = "shift.html"
-            key = request.POST.get("key")
-            cleartext = request.POST.get("cleartext")
-            print(key, cleartext)
+            if request.method == "POST":
+                count_falla = 0
+                while not encrypted:
+                    key = int(request.POST.get("key"))
+                    cleartext = request.POST.get("cleartext")
+                    print(key, cleartext)
+                    encode = encode_despla(cleartext, key, count_falla)
+                    if encode == -1:
+                        count_falla=count_falla+1
+                    else:
+                        encrypted=True
+                        context['encrypted']=True
+                        print(encode)
+                        context['encodedtext']=encode
 
-
-    context = {
-        "name" : cryptosystem_obj.name, 
-        "description" : cryptosystem_obj.description
-    }
 
     return render(request, view, context=context)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random as ran
 from math import gcd as bltin_gcd
+import re
 
 
 #///////////////////////////////////////////////////////////////
@@ -30,10 +31,13 @@ def inver_primes():
 def unify(palabra):
   palabralist = []
   palabra = palabra.replace(" ", "")
+  palabra = palabra.lower()
+  regex = re.compile('[^a-z]')
+  palabra = regex.sub('', palabra)
+  print(palabra)
   for i in range(len(palabra)):
     palabralist.append(palabra[i])
   return palabralist
-
   
 def convert (list):
   lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -58,64 +62,26 @@ def deconvert(list):
 
 
 
-
 #///////////////////////////////////////////////////////////////
 #//////////////////////METODOS DE CODIFICACION///////////////////
 #////////////////////////////////////////////////////////////////
 
-def encode_despla(palabrast):
+####LIMPIO
+def encode_despla(palabrast,key,count_falla):
   palabrals = unify(palabrast)
-  
-  for count_falla in range(4):
-    if count_falla > 2:
-      clave = ran.randint(1,26)
-      print("Al usar todos los intentos, hemos elegido una clave por usted. La clave sera: {}.".format(clave))
+  if count_falla > 2:
+    key = ran.randint(1,26)
+    palabrals = convert(palabrals)
+    for i in range(len(palabrals)):
+      palabrals[i] = (palabrals[i] + key)%26
+    palabrast = deconvert(palabrals)
+    return palabrast
+  if 1 <= key <= 26:
       palabrals = convert(palabrals)
       for i in range(len(palabrals)):
-        palabrals[i] = (palabrals[i] + clave)%26
+        palabrals[i] = (palabrals[i] + key)%26
       palabrast = deconvert(palabrals)
-      print(palabrast)
       return palabrast
-    print("ingrese una clave entre 1 y 26. \n numero de intento: {}, tiene 3 intentos.".format(count_falla+1))
-    clave = int(input())
-    if 1 <= clave <= 26:
-        palabrals = convert(palabrals)
-        for i in range(len(palabrals)):
-          palabrals[i] = (palabrals[i] + clave)%26
-        palabrast = deconvert(palabrals)
-        print(palabrast)
-        return palabrast
-    else:
-      print("clave ilegal")
-
-
-
-
-#///////////////////////////////////////////////////////////////
-#//////////////////////METODOS DE DECODIFICACION///////////////////
-#////////////////////////////////////////////////////////////////
-####mbwjebftnvzmjoeb
-####xmhupmqegzmoaeufmxuzpm (12)
-def decode_despla(string):
-  count_fallas = 0
-  while count_fallas < 3:
-    print("ingrese la clave, esta debe ser un numero entre 1 y 25.\n Tiene 3 intentos, este es el intento numero {}".format(count_fallas+1))
-    key = int(input())
-    if 1<= key<=26:
-      lista = unify(string)
-      lista = convert(lista)
-      for j in range(len(lista)):
-        lista[j] = (lista[j]-key)%26
-      string = deconvert(lista)
-      print(string)
-      return string
-    count_fallas = count_fallas + 1
-  print("Al usar los 3 intentos, la aproximacion sera por fuerza bruta, las posibilidades son las siguientes:")
-  for i in range(25):
-    lista = unify(string)
-    lista = convert(lista)
-    for j in range(len(lista)):
-      lista[j] = (lista[j]+1)%26
-    string = deconvert(lista)
-    print(string)
-  return string
+  else:
+    return -1
+  
