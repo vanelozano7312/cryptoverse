4eb7d5c015abf82f08516a982d28452b3ab4195e
Marinovsky <rmarino@unal.edu.co>
2022-10-17 12:41:56 -0500

    Blocks ciphers ready in backend and frontend

---
+++ BackendReady/SDES.py
@@ -0,0 +1,132 @@
+import numpy as np
+
+def toBinary(n, len):
+    binary = []
+    i = 1 << len - 1
+    while i > 0:
+        binary.append(1 if (n & i) else 0)
+        i = i // 2
+    return binary
+
+def GenerateKeys(key):
+    p8 = [6, 3, 7, 4, 8, 5, 10, 9]
+    p10 = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
+    # Step-1
+    key_ = [1]*10
+    for i in range(0, 10):
+        key_[i] = key[p10[i] - 1]
+    
+    # Step-2
+    l = key_[0:5]
+    r = key_[5:10]
+
+    # Step-3
+    l = list(np.roll(l, -1))
+    r = list(np.roll(r, -1))
+
+    # Step-4
+    key_ = l + r
+    k1 = [1]*8
+    for i in range(0, 8):
+        k1[i] = key_[p8[i] - 1]
+    
+    # Step-5
+    l = list(np.roll(l, -2))
+    r = list(np.roll(r, -2))
+
+    # Step-6
+    key_ = l + r
+    k2 = [1]*8
+    for i in range(0, 8):
+        k2[i] = key_[p8[i] - 1]
+    
+    return k1, k2
+
+def Function(arr, K1):
+    EP = [4, 1, 2, 3, 2, 3, 4, 1]
+    S0 = [[1, 0, 3, 2], [3, 2, 1, 0], [0, 2, 1, 3], [3, 1, 3, 2]]
+    S1 = [[0, 1, 2, 3], [2, 0, 1, 3], [3, 0, 1, 0], [2, 1, 0, 3]]
+    P4 = [2, 4, 3, 1]
+
+    # Step-2
+    l = arr[0:4]
+    r = arr[4:8]
+
+    s2 = [1]*8
+    for i in range(0, 8):
+        s2[i] = r[EP[i] - 1]
+    
+    for i in range(0, 8):
+        s2[i] = K1[i] ^ s2[i]
+    
+    rl = s2[0:4]
+    rr = s2[4:8]
+
+    # For l
+    row = int(str(rl[0]) + str(rl[3]), 2)
+    column = int(str(rl[1]) + str(rl[2]), 2)
+    s0 = toBinary(S0[row][column], 2)
+
+    # For r
+    row = int(str(rr[0]) + str(rr[3]), 2)
+    column = int(str(rr[1]) + str(rr[2]), 2)
+    s1 = toBinary(S1[row][column], 2)
+
+    s_combined = s0 + s1
+    r_p4 = [1]*4
+    for i in range(0, 4):
+        r_p4[i] = s_combined[P4[i] - 1]
+    
+    for i in range(0, 4):
+        l[i] = r_p4[i] ^ l[i]
+    
+    return l + r
+
+def Swap(arr):
+    l = arr[0:4]
+    r = arr[4:8]
+    return r + l
+
+
+def encode_sdes_text(x, K):
+    IP = [2, 6, 3, 1, 4, 8, 5, 7]
+    IP_inv = [4, 1, 3, 5, 7, 2, 8, 6]
+    
+    s1 = [1]*8
+    for i in range(0, 8):
+        s1[i] = x[IP[i] - 1]
+    
+    arr1 = Function(s1, K[0])
+    after_swap = Swap(arr1)
+    arr2 = Function(after_swap, K[1])
+    cipherText = [1]*8
+    for i in range(0, 8):
+        cipherText[i] = arr2[IP_inv[i] - 1]
+    return cipherText
+    
+def decode_sdes_text(y, K):
+    IP = [2, 6, 3, 1, 4, 8, 5, 7]
+    IP_inv = [4, 1, 3, 5, 7, 2, 8, 6]
+    
+    s1 = [1]*8
+    for i in range(0, 8):
+        s1[i] = y[IP[i] - 1]
+    
+    arr1 = Function(s1, K[1])
+    after_swap = Swap(arr1)
+    arr2 = Function(after_swap, K[0])
+    decryptedText = [1]*8
+    for i in range(0, 8):
+        decryptedText[i] = arr2[IP_inv[i] - 1]
+    return decryptedText
+
+"""
+key = [1,0,1,0,0,0,0,0,1,0]
+
+K = GenerateKeys(key, p8, p10)
+x = [1, 0, 0, 1, 0, 1, 1, 1]
+y = encode_sdes_text(x, K)
+print(y)
+x = decode_sdes_text(y, K)
+print(x)
+"""
--- cryptoverse/views.py
+++ cryptoverse/views.py
@@ -16,3 +16,29 @@ def home_view(request, *args, **kwargs):
     HTML_STRING = render_to_string("home_view.html", context=context)
    
     return HttpResponse(HTML_STRING)
+
+def classic_view(request, *args, **kwargs):
+
+    cryptosystem_qs = Cryptosystem.objects.all()
+    change_page("home")
+
+    context = {
+        "object_list" : cryptosystem_qs,
+    }
+
+    HTML_STRING = render_to_string("classic.html", context=context)
+   
+    return HttpResponse(HTML_STRING)
+
+def block_view(request, *args, **kwargs):
+
+    cryptosystem_qs = Cryptosystem.objects.all()
+    change_page("home")
+
+    context = {
+        "object_list" : cryptosystem_qs,
+    }
+
+    HTML_STRING = render_to_string("block.html", context=context)
+   
+    return HttpResponse(HTML_STRING)
