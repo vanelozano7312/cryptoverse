762ee80c332438c7b04292576a6e69c19ace8468
Juan Bernal <68033954+JuanDavinXI@users.noreply.github.com>
2022-11-22 11:41:46 -0500

    ELGAMAL CURVE CHANGE

--- BackendReady/ElGamal.py
+++ BackendReady/ElGamal.py
@@ -4,6 +4,12 @@ from sympy import randprime as rand_prime
 from sympy import isprime as isprime
 from math import gcd as bltin_gcd
 from Crypto.PublicKey import ECC
+from ecc.curve import Curve25519
+from ecc.key import gen_keypair
+from ecc.key import gen_private_key
+from ecc.key import get_public_key
+from ecc.cipher import ElGamal
+from ecc.curve import Point
 
 def prim_roots(p):
     """
@@ -264,25 +270,22 @@ def elgamal_ecc_e(text,pub_k_x,pub_k_y,count_fallas):
 
     a = -1
     if count_fallas >= 3:
-        a = random.randint(2,pow(10,50))
-        pub_k = ECC.construct(curve = 'P-256', d = a).public_key()
+        a, pub_key = gen_keypair(Curve25519)
     else:
-        try:
-            pub_k = ECC.construct(curve = 'P-256', point_x = pub_k_x, point_y = pub_k_y)
-        except ValueError:
+        pub_key = Curve25519.G
+        pub_key.x = pub_k_x
+        pub_key.y = pub_k_y
+        if not Curve25519.is_on_curve(pub_k):
             return -1,-1,-1,-1
 
-    S2 = []
-    k = random.randint(2,pow(10,50))
-    S1 = ECC.construct(curve = 'P-256', d = k).public_key()
-    for i in range(len(text)):
-        S2.append(k * pub_k.pointQ + ECC.construct(curve = 'P-256', d = ord(text[i])).pointQ)
-        S2[i] = (int(S2[i].x),int(S2[i].y))
+    cipher_elg = ElGamal(Curve25519)
+    text_b = bytes(text,'utf-8')
+    S1, S2 = cipher_elg.encrypt(text_b, pub_key)
 
-    return (S1.pointQ.x,S1.pointQ.y),S2,pub_k.pointQ.x,pub_k.pointQ.y,a
+    return (S1.x,S1.y),(S2.x,S2.y),(pub_k.x,pub_k.y),a
 
 
-def elgamal_ecc_d(s1_x,s1_y,s2,a):
+def elgamal_ecc_d(s1_x,s1_y,s2_x,s2_y,a):
     """
     Description
     -----------
@@ -306,34 +309,29 @@ def elgamal_ecc_d(s1_x,s1_y,s2,a):
         Clear decrypted text.
         (-1) if the parameters are out of bounds    
     """
-    try:
-        S1 = ECC.construct(curve = 'P-256', point_x = s1_x, point_y = s1_y)
-    except ValueError:
+
+    S1 = Curve25519.G
+    S1.x = s1_x
+    S1.y = s1_y
+
+    S2 = Curve25519.G
+    S2.x = s2_x
+    S2.y = s2_y
+
+    if not Curve25519.is_on_curve(S1):
         return -1
-
-    text = []
-
-    S = a*S1.pointQ
-    for i in range(len(s2)):
-        try:
-            C = ECC.construct(curve = 'P-256', point_x = s2[i][0], point_y = s2[i][1]).pointQ
-        except ValueError:
-            return -1
-        text.append(C + (-S))
-    for i in range(len(text)):
-        for j in range(97,123):
-            if ECC.construct(curve = 'P-256', d = j).pointQ == text[i]:
-                text[i] = chr(j)
-                break
-        if type(text[i]) != type(''):
-            text[i] = ' '
+    if not Curve25519.is_on_curve(S2):
+        return -1
     
-    return ''.join(text)
+    cipher_elg = ElGamal(Curve25519)
+    text = cipher_elg.decrypt(a,S1,S2)
+    return text.decode('utf-8')
 
 
 a = 15
 pr_key = ECC.construct(curve='P-256',d=a)
 pub_key = pr_key.public_key()
+
 
 k=  12984129847129
 M = 12
@@ -351,28 +349,23 @@ for i in range(1,25):
         print(i)
         break
 
-# p = rand_prime(pow(2,24),pow(2,26))
-# g = prim_roots(p)
-# a = random.randint(2,2^10)
-# h = power(g,a,p)
-# print(p, g, a, h)
-# print(elgamal_enc('askjfhaskjf',p,g,h,1))
-
-# p = 42004813
-# g = 7597416
-# a = random.randint(2,2^10)
-# h = 28025900
-
-# print(elgamal_enc('askjfhaskjf', p, g, h, 0))
-
-# p=39577913
-# a=5
-# s1=15151015
-# text="[500024720, 533680230, 519256440, 466369210]"
-# text = str_to_list(text)
-# print(elgamal_dec(text, s1, a, p))
-
 s11=101677601383488996603075606535380255913010825269751815769841498664861664196811
 s12=13773740408660364011136485548507825733693522803178641816330243385508794177318
 s2="[(100496040572723822934014757929413114100665887872085620547350405136730667827870, 11891784355483902878420332758254726272363245351700905244072721670011843095230), (8459579126204352643407501919024558721938392553927414518975256116869977596062, 15331667878075208381306875463013169571040001120005414383843820657260996309916), (63585599298583948292450817100721955471936610691296952417434272025052465189539, 115594602241206645993104028328760770511057139783849136013195948214649174654810), (88676361745046123069790464985751564899090966340535587979919104072725408960877, 48085515339909112600553883066276515839924106387251701126694562793231124542056), (108868155680714357461498365085860541962634841613942544335336385814513262407664, 64747336522822438799697023904881487219791681139116515168766305265359278982931), (103687670218257693414125117510826151548340956699690369706564851224691769253617, 4096153036426876202722232016627631216992890943478348602103866440979888546193)]"
 a= 12061096845004200634431039739511748362955237408440
+
+
+text = "texto"
+a = 15
+elgama = ElGamal(Curve25519)
+ptk = get_public_key(a,Curve25519)
+bubu = Curve25519.G
+
+C1,C2 = elgama.encrypt(bytes(text,'utf-8'),ptk)
+print(elgama.decrypt(a,C1,C2))
+pub_k = get_public_key(a,Curve25519)
+ret = elgamal_ecc_e(text,pub_k.x,pub_k.y,0)
+
+
+
+print(elgamal_ecc_d(ret[0][0],ret[0][1],ret[1][0],ret[1][1],a))
