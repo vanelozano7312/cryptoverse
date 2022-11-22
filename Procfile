8a140f2132e807d5e8ddca24b8e600e612ff09a8
Sergio Prieto <60900923+seprietov@users.noreply.github.com>
2022-11-21 21:27:13 -0500

    is it ready for public

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cryptoverse.wsgi --log-file -
--- cryptoverse/settings.py
+++ cryptoverse/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8xq^6^2fsaj3zko-t+8+wfy=m4(41guap6z5@6v74dp6lfz7e=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,9 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR/'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
